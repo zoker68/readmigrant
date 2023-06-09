e64ad6d1d5597dcae57a2209ae32ced7064dadb2
ZoKeR <zoker68@ya.ru>
2023-06-09 10:37:19 +0200

    counter v2

--- resources/js/Layouts/FooterLayout.vue
+++ resources/js/Layouts/FooterLayout.vue
@@ -4,6 +4,23 @@
             <p>© 2023 ReadMigrant. Все права защищены.</p>
             <p>Для связи со мной можете использовать почту info собака readmigrant точка com</p>
         </div>
+
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript" >
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(93902323, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true
+            });
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/93902323" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
     </footer>
 </template>
 
