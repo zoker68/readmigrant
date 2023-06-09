afbeec8ec4f70f048244141d982318e5a7fed49b
ZoKeR <zoker68@ya.ru>
2023-06-09 10:00:02 +0200

    Logo link

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,8 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
 
         <!-- Scripts -->
         @routes
